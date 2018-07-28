c4d23dd838a4417c7aa37d9212052275b08a7f5b
Sunshine Daydream <alexbruner@live.com>
2018-07-28 15:27:13 -0500

    page ready to begin editing

--- server.js
+++ server.js
@@ -7,8 +7,29 @@ var exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 3000;
 var app = express();
 
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Routes
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Run Server
 db.sequelize.sync().then(function(){
   app.listen(PORT, function(){
     console.log("Server listening on port:", PORT);
   });
 });
+
+module.exports = app;
